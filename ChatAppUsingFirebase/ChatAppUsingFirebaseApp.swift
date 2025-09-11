a50dbd0b94a46ec87ec649dc468370b61337dfba
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-09-11 17:22:11 +0530

    Initial commit
    First commit

---
+++ ChatAppUsingFirebase/ChatAppUsingFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppUsingFirebaseApp.swift
+//  ChatAppUsingFirebase
+//
+//  Created by Neosoft on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppUsingFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
